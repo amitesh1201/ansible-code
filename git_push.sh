cc23528573da8bdf659610fc3faf6115185c9745
Swapnil <swapnil@MacBook-Pro-swapnilzarbade.local>
2018-03-24 13:45:46 +0530

    Added if condition to check the variables are empty or not.

--- git_push.sh
+++ git_push.sh
@@ -17,7 +17,13 @@ do
     esac
 done;
 
-git add /Users/swapnil/ansible-code
-cd /Users/swapnil/ansible-code
-git commit -m "$git_msg"
-git push origin "$git_branch"
+if [[ ! -z $git_msg ]] && [[ ! -z $git_branch ]]; then
+    git add /Users/swapnil/ansible-code
+    cd /Users/swapnil/ansible-code
+    git commit -m "$git_msg"
+    git push origin "$git_branch"
+else
+    echo "Specify the parameter.Check the following example."
+    echo "Example: sh git_push.sh -m <"Enter commit msg" -d <branch_name>>"
+    exit 1
+fi
